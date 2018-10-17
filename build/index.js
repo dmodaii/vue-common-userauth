7b975709a54c4046e3e3258de903c81c147426d8
zhiming.dai <zhiming.dai@jyblife.com>
2018-10-17 19:02:14 +0800

    #000

--- build/index.js
+++ build/index.js
@@ -14,6 +14,8 @@
 }
  */
 
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
 function hasAuth(module, action, userAuthAll) {
   if (!module) {
     return false;
@@ -34,16 +36,44 @@ function hasAuth(module, action, userAuthAll) {
 
 function nodeHasAuth(module, action) {
   var self = this;
-  return async function (ctx, next) {
-    // this is instance of Context
-    var userAuthAll = ctx.session.passportUserAuth || {};
-    if (hasAuth(module, action, userAuthAll)) {
-      await next();
-    } else {
-      // https://tools.ietf.org/html/rfc2616#page-66
-      ctx.status = 401; // 'Unauthorized'
-    }
-  };
+  return function () {
+    var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(ctx, next) {
+      var userAuthAll;
+      return regeneratorRuntime.wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              // this is instance of Context
+              userAuthAll = ctx.session.passportUserAuth || {};
+
+              if (!hasAuth(module, action, userAuthAll)) {
+                _context.next = 6;
+                break;
+              }
+
+              _context.next = 4;
+              return next();
+
+            case 4:
+              _context.next = 7;
+              break;
+
+            case 6:
+              // https://tools.ietf.org/html/rfc2616#page-66
+              ctx.status = 401; // 'Unauthorized'
+
+            case 7:
+            case 'end':
+              return _context.stop();
+          }
+        }
+      }, _callee, this);
+    }));
+
+    return function (_x, _x2) {
+      return _ref.apply(this, arguments);
+    };
+  }();
 }
 
 function setAuth() {}
--- index.js
+++ index.js
@@ -1,19 +1,5 @@
 'use strict';
 
-// window.userAuth
-/**
- * {
- *  
-    collectask: {
-         id: "22", 
-         code: "collectask", 
-         actions: "index,detail,add,edit,save,del", 
-         actionsList: (6) ["index", "detail", "add", "edit", "save", "del"]
-        code: "collectask"
-        id: "22"
-}
- */
-
 function hasAuth(module, action, userAuthAll) {
   if(!module) {
     return false;
